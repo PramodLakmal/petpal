65e193ca5760ba42f543be0e4e07de3635c279c1
it22589286 <it22589286@my.sliit.lk>
2024-10-05 15:35:05 +0530

    created user sign up and registration

---
+++ lib/user registration/SignUp.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:petpal/user%20registration/homeScreen.dart';
+import 'package:petpal/user%20registration/login.dart';
+import 'package:petpal/user%20registration/services/authentication.dart';
+import 'package:petpal/user%20registration/widget/button.dart';
+import 'package:petpal/user%20registration/widget/showSnackbar.dart';
+import 'package:petpal/user%20registration/widget/text_field.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
+  bool isLoading = false;
+
+  void despose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    nameController.dispose();
+  }
+
+  void signUpUser() async {
+    String res = await AuthServices().signUpUser(
+        email: emailController.text,
+        password: passwordController.text,
+        name: nameController.text);
+
+    if (res == "Success") {
+      setState(() {
+        isLoading = true;
+      });
+
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => HomeScreen()));
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+
+      showSnackBar(context, res);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: height / 2.7,
+            child: Image.asset("images/v.png"),
+          ),
+          TextFieldInput(
+              hintText: "name",
+              icon: Icons.person,
+              textEditingController: nameController),
+          TextFieldInput(
+              hintText: "email",
+              icon: Icons.email,
+              textEditingController: emailController),
+          TextFieldInput(
+              hintText: "password",
+              icon: Icons.lock,
+              textEditingController: passwordController,
+              isPass: true),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                "Forgot Password?",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          MyButton(onTab: signUpUser, text: "Sign Up"),
+          SizedBox(height: height / 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Already have an account?"),
+              GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.blue),
+                  ))
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
---
+++ lib/user registration/widget/text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TextFieldInput extends StatelessWidget {
+ 
+  final TextEditingController textEditingController;
+  final bool isPass;
+  final String hintText;
+  final IconData? icon;
+  
+
+   const TextFieldInput(
+      {super.key,
+      this.isPass = false,
+      required this.hintText,
+      required this.icon,
+      
+      required this.textEditingController});
+      
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextField(
+        obscureText: isPass,
+        controller: textEditingController,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.black),
+          prefixIcon:  Icon(icon, color: Colors.black),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          border: InputBorder.none,
+          filled: true,
+          fillColor: Color(0xffEFEFEF),
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(10)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(width: 1, color: Colors.black),
+              borderRadius: BorderRadius.circular(30)),
+        ),
+      ),
+    );
+  }
+}
