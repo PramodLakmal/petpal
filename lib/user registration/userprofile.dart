5a0f578a11de2374da723a3ced96434c0212922a
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-10-08 16:52:30 +0530

    Refactor userprofile.dart and pubspec.lock

--- lib/user registration/userprofile.dart
+++ lib/user registration/userprofile.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
-import '../Screens/newsfeed/news_feed_screen.dart';
 
 import 'package:petpal/user%20registration/login.dart';
 
